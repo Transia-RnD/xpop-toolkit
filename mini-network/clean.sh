9a221b258ccc48d76a74bd0e89ba873722ed8a62
Denis Angell <dangell@transia.co>
2023-07-12 18:35:49 +0200

    update clean script

--- mini-network/clean.sh
+++ mini-network/clean.sh
@@ -1,7 +1,7 @@
 docker compose -f docker-compose.yml down --remove-orphans
 # Clean Burn Chain
 rm -r burn_vmaster/lib
-rm -r burn_vmaster/lob
+rm -r burn_vmaster/log
 rm -r burn_vnode1/lib
 rm -r burn_vnode1/log
 rm -r burn_pnode1/lib
